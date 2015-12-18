d5e8b2bc7ad601fae4d0b2288c8081ffb7bd5f3a
neTp9c <alexpetryakov@gmail.com>
2015-12-18 14:38:50 +0300

    Initial commit

---
+++ Elements/ShareButtons.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Orchard.Layouts.Framework.Elements;
+
+namespace MainBit.ShareButtons.Elements
+{
+    public class ShareButtons : Element
+    {
+        public override string Category
+        {
+            get { return "Social"; }
+        }
+    }
+}
